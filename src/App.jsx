3317be028f705f2acab9022ff7d2b3c54e1a337f
Ruslan <axelezgames@gmail.com>
2023-07-27 19:42:13 +0500

    add simple template for menu

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./style/style.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center mt-2">
+        <button
+          type="button"
+          className="px-5 py-2.5 text-white text-center rounded-lg bg-gradient-to-bl from-purple-500 to-pink-400"
+        >
+          Home
+        </button>
+      </div>
+      <div className="grid ">hi</div>
+    </>
+  );
+}
+
+export default App;
